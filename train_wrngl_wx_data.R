54925950c006ae3859731628cd2af69bd822e6b9
Jill <jillian5521@gmail.com>
2024-07-11 21:27:45 -0600

    filtering of wx data utilizing dplyr

---
+++ train_wrngl_wx_data.R
@@ -0,0 +1,9 @@
+
+raw_wx <- read.csv("https://de.cyverse.org/anon-files//iplant/home/shared/commons_repo/curated/GenomesToFields_GenotypeByEnvironment_PredictionCompetition_2023/Testing_data/4_Testing_Weather_Data_2022.csv", na.strings = c("", "NA"))
+
+cols_for_removal <- c("GWETPROF","GWETROOT","ALLSKY_SFC_PAR_TOT", "ALLSKY_SFC_SW_DNI")
+
+library(dplyr)
+clean_wx <- raw_wx %>% select(-all_of(cols_for_removal))
+
+
