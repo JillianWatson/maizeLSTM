b4b7aba944873e16bb0af460ac5de2377d4ce63b
Jill <jillian5521@gmail.com>
2024-12-25 00:41:53 -0600

    arranged data based on env, imputed NA values

---
+++ model/build_model.R
@@ -0,0 +1,18 @@
+library(keras)
+library(tensorflow)
+
+lstm <- keras_model_sequential()
+
+# to be determined
+features <- c()
+look_back <- 12
+forecast_horizon <- 1
+
+input_shape <- c(look_back, length(features))
+
+
+
+
+
+             
+
